8be57a1550cb3b9bb726c648a7818ef040bb2376
jhael07 <109443811+jhael07@users.noreply.github.com>
2023-01-07 12:04:47 -0400

    leet see

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ export default () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src={"./images/troll.svg"} />{" "}
+        <img src={"/images/troll.svg"} />{" "}
         <h2>
           Meme Generator <span className="credit">By Jhael Rodriguez</span>
         </h2>
